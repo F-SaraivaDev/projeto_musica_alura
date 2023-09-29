1937c2fde2a0b8cd24be1ad3a37745b83faaeff8
Fernando de Souza Saraiva - PrestServ <fernando.saraiva.prestserv@petrobras.com.br>
2023-09-29 15:55:06 -0300

    Projeto Musica Alura

---
+++ src/br/com/alura/minhasmusicas/App.java
@@ -0,0 +1,33 @@
+package br.com.alura.minhasmusicas;
+
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcos Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+    }
+}
